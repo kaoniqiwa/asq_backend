3b12c9f036c7d7e9733b0d6fd0591fa7b724e95e
panminxiang <1061886912@qq.com>
2022-07-27 15:15:40 +0800

    pmx

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { RoutePath } from './enum/route-path.enum';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: RoutePath.login,
+    pathMatch: 'full'
+  },
+  {
+    path: RoutePath.login,
+    component: LoginComponent
+  },
+  {
+    path: RoutePath.neoballoon,
+    loadChildren: () =>
+      import('./neoballoon-system/neoballoon.module').then((mod) => mod.NeoballoonModule),
+  }
+]
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/enum/route-path.enum.ts
@@ -0,0 +1,4 @@
+export enum RoutePath {
+  login = 'login',
+  neoballoon = "neoballoon"
+}
