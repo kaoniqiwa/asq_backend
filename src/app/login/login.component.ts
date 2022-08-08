7a662f2c11211b5ab74bf846ef137926d4e5229c
panminxiang <1061886912@qq.com>
2022-08-08 10:24:44 +0800

    pmx

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.less']
+  styleUrls: ['./login.component.less'],
+  host: {
+    "class": "login-content"
+  }
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  myForm = this._fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+  constructor(private _fb: FormBuilder,) { }
 
   ngOnInit(): void {
   }
